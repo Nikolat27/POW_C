e8d00e35e69c45f51f053069600373ac39f80497
nikolat27 <samalizadeh899@gmail.com>
2025-10-25 00:10:40 +0330

    Added mining

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define SHA_256_LEN 32
+#define SHA_256_HEX_LEN SHA_256_LEN * 2
+#define POW_DIFFICULTY 3
 
 typedef struct
 {
   int id;
   uint8_t prevHash[SHA_256_LEN];
   uint8_t hash[SHA_256_LEN];
+  int nonce;
   int timestamp;
 } Block;
 
@@ -21,59 +25,106 @@ typedef struct
   int size;
 } Blockchain;
 
-void calculateHash(Block *b)
+char *hexHash(uint8_t *hash)
+{
+  char *hexedHash = malloc(65);
+  hexedHash[0] = '\0';
+
+  for (int i = 0; i < SHA_256_LEN; i++)
+  {
+    char buffer[3];
+    sprintf(buffer, "%02x", hash[i]);
+
+    strcat(hexedHash, buffer);
+  }
+
+  return hexedHash;
+}
+
+bool hasPrefix(char *str, int difficulty)
+{
+  char prefix[difficulty];
+
+  memset(&prefix, '0', difficulty);
+
+  return strncmp(str, prefix, difficulty) == 0;
+}
+
+// POW -> Mining
+bool POW(Block *block, char *prevHashHex, char *hash, int difficulty)
+{
+  char buffer[1024];
+
+  sprintf(buffer, "%d-%s-%d-%d", block->id, prevHashHex, block->nonce, block->timestamp);
+  SHA256((const unsigned char *)buffer, strlen(buffer), hash);
+
+  char *hexedHash = hexHash(hash);
+
+  if (hasPrefix(hexedHash, difficulty))
+  {
+    free(hexedHash);
+    return true;
+  }
+
+  free(hexedHash);
+  return false;
+}
+
+void calculateHash(Block *block, int difficulty)
 {
   unsigned char hash[SHA_256_LEN];
-  char buffer[1024];
   char prevHashHex[SHA_256_LEN * 2 + 1]; // Hex string for prevHash
 
   for (int i = 0; i < SHA_256_LEN; i++)
   {
-    sprintf(prevHashHex + (i * 2), "%02x", b->prevHash[i]);
+    sprintf(prevHashHex + (i * 2), "%02x", block->prevHash[i]);
   }
   prevHashHex[SHA_256_LEN * 2] = '\0';
 
-  sprintf(buffer, "%d-%s-%d", b->id, prevHashHex, b->timestamp);
+  while (true)
+  {
+    bool mined = POW(block, prevHashHex, hash, difficulty);
+    if (mined)
+    {
+      printf("Block Mined! Nonce: %d\n", block->nonce);
+      break;
+    }
 
-  SHA256((const unsigned char *)buffer, strlen(buffer), hash);
+    printf("Block didn`t mine, Nonce: %d\n", block->nonce);
+    block->nonce++;
 
-  memcpy(b->hash, hash, SHA_256_LEN);
+    continue;
+  }
+
+  memcpy(block->hash, hash, SHA_256_LEN);
 }
 
-Block *createGenesisBlock()
+Block *createGenesisBlock(int difficulty)
 {
   Block *genesisBlock = malloc(sizeof(Block));
   if (!genesisBlock)
   {
     printf("failed to malloc for genesisBlock\n");
+    free(genesisBlock);
     return NULL;
   }
 
   genesisBlock->id = 1;
+  genesisBlock->nonce = 0;
 
   genesisBlock->timestamp = (int)time(NULL);
 
   memset(genesisBlock->prevHash, 0, SHA_256_LEN);
   memset(genesisBlock->hash, 0, SHA_256_LEN);
 
-  calculateHash(genesisBlock);
+  calculateHash(genesisBlock, difficulty);
 
   return genesisBlock;
 }
 
-void printHash(uint8_t *hash)
+Blockchain *newBlockchain(int difficulty)
 {
-  for (int i = 0; i < SHA_256_LEN; i++)
-  {
-    printf("%02x", hash[i]);
-  }
-
-  printf("\n");
-}
-
-Blockchain *newBlockchain()
-{
-  Block *genesisBlock = createGenesisBlock();
+  Block *genesisBlock = createGenesisBlock(difficulty);
   if (!genesisBlock)
     return NULL;
 
@@ -91,7 +142,7 @@ Blockchain *newBlockchain()
   return bc;
 }
 
-Block *newBlock(Blockchain *bc)
+Block *newBlock(Blockchain *bc, int difficulty)
 {
   Block *previousBlock = bc->blocks[bc->size - 1];
 
@@ -99,6 +150,7 @@ Block *newBlock(Blockchain *bc)
   if (!newBlock)
   {
     printf("FAILED to malloc for new block\n");
+    free(newBlock);
     return NULL;
   }
 
@@ -108,7 +160,7 @@ Block *newBlock(Blockchain *bc)
   memcpy(newBlock->hash, previousBlock->hash, SHA_256_LEN);
   memset(newBlock->hash, 0, SHA_256_LEN);
 
-  calculateHash(newBlock);
+  calculateHash(newBlock, difficulty);
 
   return newBlock;
 }
@@ -135,13 +187,13 @@ void freeBlockchain(Blockchain *bc)
 
 int main()
 {
-  Blockchain *bc = newBlockchain();
+  Blockchain *bc = newBlockchain(POW_DIFFICULTY);
 
-  Block *block = newBlock(bc);
+  Block *block = newBlock(bc, POW_DIFFICULTY);
 
-  int newSize = addBlock(bc, block);
+  int newBlockchainSize = addBlock(bc, block);
 
-  printf("%d\n", newSize);
+  printf("%d\n", newBlockchainSize);
 
   freeBlockchain(bc);
   return 0;
